b8d5b5721caf18104541da9c3087772c3b98d8c9
Quang Bảo <62896400+baolachum100@users.noreply.github.com>
2021-06-23 13:21:52 +0700

    Add files via upload

---
+++ Chapter 1/BaiTap4/BaiTap4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BaiTap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** PersonalInfo ****");
+            Console.WriteLine("Name: PersonalInfo ");
+            Console.WriteLine("Date of birth: 12/12/1980");
+            Console.WriteLine("Phone Job: 0123456789");
+            Console.WriteLine("Phone numbers: 0987654321");
+            Console.ReadKey();
+        }
+    }
+}
