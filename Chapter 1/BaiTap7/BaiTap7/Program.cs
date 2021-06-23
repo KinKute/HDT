5c742857accbfec8b3619d1c61e7799ace5cf22b
Quang Bảo <62896400+baolachum100@users.noreply.github.com>
2021-06-23 13:25:46 +0700

    Add files via upload

---
+++ Chapter 1/BaiTap7/BaiTap7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BaiTap7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   XXXXXX   \n"+
+                              " X        X \n"+
+                              "X   STOP   X\n"+
+                              " X        X\n"+
+                              "   XXXXXX\n"+
+                              "      X  \n"+
+                              "      X\n"+
+                              "      X");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter 1/BaiTap8/BaiTap8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BaiTap8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BigLetter");
+            Console.WriteLine("T");
+            Console.WriteLine("TTTTTTTTTT\n"
+                             +"    T\n"
+                             +"    T \n"
+                             +"    T \n"
+                             +"    T\n"
+                             +"    T\n"+
+                              "    T\n");
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
