7dd3cada79040f80d3050d798a7b5365bee14c83
Quang Bảo <62896400+baolachum100@users.noreply.github.com>
2021-06-23 14:15:27 +0700

    Add files via upload

---
+++ Lop_Doi_Tuong_Bai02/Lop_Doi_Tuong_Bai02/DanhSachSinhVien.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace Lop_Doi_Tuong_Bai02
+{
+    class DanhSachSinhVien
+    {
+        List<SinhVien> ds = new List<SinhVien>();
+        public void Them(SinhVien sv)
+        {
+            ds.Add(sv);
+        }
+        public void NhapTuFile()
+        {
+            var filename = "data.txt";
+            StreamReader sr = new StreamReader(filename);
+            var line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                Them(new SinhVien(line));
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var sv in ds)
+            {
+                sb.Append(sv + "\n");
+            }
+            return sb.ToString();
+        }
+        private int DemSLSVTheoGTVaLop(string GT, string lop)
+        {
+            int dem = 0;
+            foreach (var sv in ds)
+            {
+                if (sv.gioiTinh == GT)
+                    dem++;
+            }
+            return dem;
+        }
+        public int DemSLSVNam()
+        {
+            int dem = 0;
+            foreach (var item in ds)
+            {
+                if (item.gioiTinh.Trim().CompareTo("NAM".Trim()) == 0)
+                    dem++;
+            }
+            return dem;
+        }
+        public int DemSLSVNu()
+        {
+            int dem = 0;
+            foreach (var item in ds)
+            {
+                if (item.gioiTinh.Trim().CompareTo("NU".Trim()) == 0)
+                    dem++;
+            }
+            return dem;
+        }
+        public enum KieuSapXep
+        {
+            tangDTB,
+            giamDTB
+        }
+        public DanhSachSinhVien SapXepTheoDiemTrungBinhTang()
+        {
+            DanhSachSinhVien kq = new DanhSachSinhVien();
+            SinhVien tam;
+            for (int i = 0; i < ds.Count; i++)
+            {
+                for (int j = i + 1; j < ds.Count; j++)
+                    if (ds[i].dTB >= ds[j].dTB)
+                    {
+                        tam = ds[i];
+                        ds[i] = ds[j];
+                        ds[j] = tam;
+
+                    }
+                kq.Them(ds[i]);
+            }
+            return kq;
+        }
+        public DanhSachSinhVien SapXepTheoDiemTrungBinhGiam()
+        {
+            DanhSachSinhVien kq = new DanhSachSinhVien();
+            SinhVien tam;
+            for (int i = 0; i < ds.Count; i++)
+            {
+                for (int j = i + 1; j < ds.Count; j++)
+                    if (ds[i].dTB <= ds[j].dTB)
+                    {
+                        tam = ds[i];
+                        ds[i] = ds[j];
+                        ds[j] = tam;
+
+                    }
+                kq.Them(ds[i]);
+            }
+            return kq;
+        }
+        public double TimDTBCaoNhat()
+        {
+            return ds.Max(x => x.dTB);
+        }
+        public DanhSachSinhVien TimDSSVCoDTBCaoNhat()
+        {
+            DanhSachSinhVien kq = new DanhSachSinhVien();
+            double max = TimDTBCaoNhat();
+            foreach (var item in ds)
+            {
+                if (item.dTB == max)
+                    kq.Them(item);
+            }
+            return kq;
+        }
+        public DanhSachSinhVien LopCoSinhVienCoDiemTrungBinhCaoNhat()
+        {
+            DanhSachSinhVien kq = new DanhSachSinhVien();
+            double max = TimDTBCaoNhat();
+            for (int i = 0; i < ds.Count; i++)
+            {
+                if (ds[i].dTB == max)
+                {
+                    Console.WriteLine("Sinh vien co diem trung binh cao nhat thuoc lop: {0} ", ds[i].lop);
+                    kq.Them(ds[i]);
+                }
+            }
+            return kq;
+        }
+
+        public double TimDTBThapNhat()
+        {
+            return ds.Min(x => x.dTB);
+        }
+        public DanhSachSinhVien LopCoSinhVienKhongCoDiemTrungBinhCaoNhat()
+        {
+            DanhSachSinhVien kq = new DanhSachSinhVien();
+            double max = TimDTBThapNhat();
+            for (int i = 0; i < ds.Count; i++)
+            {
+                if (ds[i].dTB == max)
+                {
+                    Console.WriteLine("Sinh vien khong co diem trung binh cao nhat thuoc lop: {0} ", ds[i].lop);
+                    kq.Them(ds[i]);
+                }
+            }
+            return kq;
+        }
+        public DanhSachSinhVien HienThiDanhSachSinhVienTheoLopvaDtb()
+        {
+            DanhSachSinhVien kq = new DanhSachSinhVien();
+            SinhVien tam;
+            for (int i = 0; i < ds.Count; i++)
+            {
+                for (int j = i + 1; j < ds.Count; j++)
+                {
+                    if (ds[i].lop.CompareTo(ds[j].lop) > 0)
+                    {
+                        tam = ds[i];
+                        ds[i] = ds[j];
+                        ds[j] = tam;
+                    }
+                }
+                kq.Them(ds[i]);
+            }
+            return kq;
+        }
+    }
+}
---
+++ Lop_Doi_Tuong_Bai02/Lop_Doi_Tuong_Bai02/SinhVien.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lop_Doi_Tuong_Bai02
+{
+    class SinhVien
+    {
+        public string maSV;
+        public string hoTen;
+        public float dTB;
+        public string lop;
+        public string gioiTinh;
+        public string Lop
+        {
+            get { return lop; }
+            set { lop = value.Trim(); }
+        }
+        //
+        public SinhVien()
+        {
+            maSV = "1";
+            hoTen = "a";
+        }
+        public SinhVien(string ma,string ho,float dtb,string lop,string gt)
+        {
+            maSV = ma;
+            hoTen = ho;
+            dTB = dtb;
+            this.lop = lop;
+            gioiTinh = gt;
+        }
+        public SinhVien(string line)
+        {
+            string[] str = line.Split(',');
+            maSV = str[0];
+            hoTen = str[1];
+            dTB = float.Parse(str[2]);
+            lop = str[3];
+            gioiTinh = str[4];
+        }   
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap ma so sinh vien: ");
+            maSV = Console.ReadLine();
+            Console.WriteLine("Nhap ho ten: ");
+            hoTen = Console.ReadLine();
+            Console.WriteLine("Nhap diem trung binh: "); ;
+            float.TryParse(Console.ReadLine(), out dTB);
+            Console.WriteLine("Nhap lop: ");
+            lop = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return string.Format("{0, 2} {1, 10} {2,5} {3, 6} {4, 10}", maSV, hoTen, dTB, gioiTinh , lop);
+        }
+    }
+    
+}
