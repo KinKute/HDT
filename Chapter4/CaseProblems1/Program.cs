36b4a55e7338a62c0e8c8f4bed0a1f63a88b3b14
Quang Bảo <62896400+baolachum100@users.noreply.github.com>
2021-06-23 13:56:12 +0700

    Add files via upload

---
+++ Chapter4/CaseProblems1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CaseProblems1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GreenvilleRevenue");
+            int a, b;
+            Console.WriteLine("Nhap so luong thi sinh nam ngoai : ");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Nhap so luong thi sinh nam nay : ");
+            int.TryParse(Console.ReadLine(), out b);
+            if (b > a)
+            {
+                if (b / 2 == a)
+                    Console.WriteLine("Cuoc thi nam nay lon hon gap doi!");
+                else
+                    Console.WriteLine("Cuoc thi lon hon bao gio het!");
+            }
+            else
+                Console.WriteLine("Mot cuoc dua chat che hon trong nam nay! Hay di ra va bo phieu cua ban!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter4/CaseProblems2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CaseProblems2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int tuongTrong, tuongNgoai, tongChiPhi;
+            byte thang;
+            Console.WriteLine("Nhap vao so buc tranh tuong Noi That can son : ");
+            int.TryParse(Console.ReadLine(), out tuongTrong);
+            Console.WriteLine("Nhap vao so buc tranh tuong Ngoai can son : ");
+            int.TryParse(Console.ReadLine(), out tuongNgoai);
+            Console.WriteLine("Nhap thang ");
+            byte.TryParse(Console.ReadLine(), out thang);
+
+            if (thang == 12 || thang == 1 || thang == 2)
+            {
+                tuongNgoai = 0;
+                tongChiPhi = 750 * tuongNgoai + 500 * tuongTrong;
+                Console.WriteLine("Tong chi phi la: ${0}", tongChiPhi);
+            }
+            else if (thang == 4 || thang == 5 || thang == 9 || thang == 10)
+            {
+                tongChiPhi = 699 * tuongNgoai + 500 * tuongTrong;
+                Console.WriteLine("Tong chi phi la: ${0}", tongChiPhi);
+            }
+            else if (thang == 7 || thang == 8)
+            {
+                tongChiPhi = 750 * tuongNgoai + 450 * tuongTrong;
+                Console.WriteLine("Tong chi phi la: ${0}", tongChiPhi);
+            }
+            else
+            {
+                tongChiPhi = 500 * tuongNgoai + 750 * tuongTrong;
+            }
+            Console.ReadLine();
+        }
+    }
+}
