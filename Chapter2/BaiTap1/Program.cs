ac4e24804d054f8868718764969e945f2438fb0a
Quang Bảo <62896400+baolachum100@users.noreply.github.com>
2021-06-23 13:32:40 +0700

    Add files via upload

---
+++ Chapter2/BaiTap1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BaiTap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, e, f, g, h, i, j, k, l;
+            a = 2 + 5 * 3;
+            Console.WriteLine("a) 2 + 5 * 3 = " + a);
+            b = (double)9 / 4 + 10;
+            Console.WriteLine("b) 9 / 4 + 10 = " + b);
+            c = (double)10 / 3;
+            Console.WriteLine("c) 10 / 3 = " + c);
+            d = 21 % 10;
+            Console.WriteLine("d) 21 % 10 = " + d);
+            e = (5 - 1) * 3;
+            Console.WriteLine("e) (5 - 1) * 3 = " + e);
+            f = (double)37 / 5;
+            Console.WriteLine("f) 37 / 5 = " + f);
+            g = 64 % 8;
+            Console.WriteLine("g) 64 % 8 = " + g);
+            h = 5 + 2 * 4 - 3 * 4;
+            Console.WriteLine("h) 5 + 2 * 4 - 3 * 4 = " + h);
+            i = (double)3 * (2 + 5) / 5;
+            Console.WriteLine("i) 3 * (2 + 5) / 5 = " + i);
+            j = 28 % 5 - 2;
+            Console.WriteLine("j) 28 % 5 - 2 = " + j);
+            k = (double)19 / 2 / 2;
+            Console.WriteLine("k) 19 / 2 / 2 = " + k);
+            l = (double)28 / (2 + 4);
+            Console.WriteLine("l) 28 / (2 + 4) = " + l);
+            Console.ReadLine();
+        }
+    }
+}
