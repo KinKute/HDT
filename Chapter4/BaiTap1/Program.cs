19de54f53cef8d458f928e75471acee7639aa824
Quang Bảo <62896400+baolachum100@users.noreply.github.com>
2021-06-23 13:50:35 +0700

    Add files via upload

---
+++ Chapter4/BaiTap1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BaiTap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string strga;
+            Console.WriteLine("CheckCredit");
+            Console.Write("Nhap gia tri cho mat hang: ");
+            strga = Console.ReadLine();
+            int.TryParse(strga, out a);
+            string kq = (a < 8000) ? ("Approved!") : ("Error, please try again!!!");
+            Console.WriteLine(kq);
+            Console.ReadLine();
+        }
+    }
+}
