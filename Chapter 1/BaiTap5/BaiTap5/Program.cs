b41bb2af4b99d5b5a7a09e0e808882ce0d5fe32a
Quang Bảo <62896400+baolachum100@users.noreply.github.com>
2021-06-23 13:24:06 +0700

    Add files via upload

---
+++ Chapter 1/BaiTap5/BaiTap5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Text.Unicode;
+
+namespace BaiTap5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("***************** MY FAVORITE SONG! *****************\n" +
+                "   ***************** LYRICS *****************\n" +
+                "  ** Khi đôi môi em còn đỏ mọng\n+" +
+                " ** Em muốn nói em yêu anh\n"+
+                "  ** Khi men còn trong hơi thở\n"+
+                "  ** Lại gần hôn anh đi");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter 1/BaiTap6/BaiTap6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace BaiTap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //lệnh xuất được tiếng việt có dấu
+            Console.WriteLine("****** COMMENTS ******");
+            Console.WriteLine("Hello World!");
+            //xuất ra màn hình dòng chữ Hello World!
+            Console.Write("Nhập tên của bạn vào: ");
+            Console.WriteLine("Tên của bạn là: " + Console.ReadLine());
+            /*
+             -viết ra màn hình dòng chữ "Nhập tên của bạn vào:" sau đó cho người dùng nhập vào
+            - xuất ra màn hình "Tên của bạn là:" kèm theo tên mà người dùng nhập
+             */
+            Console.ReadKey();
+            //lệnh dừng màn hình, chờ ấn 1 phím để thoát chương trình
+        }
+    }
+}
